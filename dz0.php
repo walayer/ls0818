a0c38a93cce420b23701180d15e311bd50b298b2
Your Name <you@example.com>
2018-08-14 00:50:33 +0600

    var_2

--- dz0.php
+++ dz0.php
@@ -1,13 +1,9 @@
 <?php
 
 const TESTCONSTANTA = 'asd';
-$user_name = "Igor";
 
 function show_something()
 {
     return 'SOMETHING';
 }
 
-if (1==1) {
-    echo 'hi';
-}
---
+++ dz0_2.php
@@ -0,0 +1,7 @@
+<?php
+
+$user_name = "Igor";
+
+if (1==1) {
+    echo 'hi';
+}
