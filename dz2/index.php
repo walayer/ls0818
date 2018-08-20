5519f345ac593fe8b7d12eb033702f72f8337f8b
Your Name <you@example.com>
2018-08-20 20:58:25 +0600

    Домашнее задание 2

---
+++ dz2/index.php
@@ -0,0 +1,140 @@
+<pre>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: walay
+ * Date: 20.08.2018
+ * Time: 14:14
+ */
+
+require('src/functions.php');
+
+/** Задание 1 */
+echo '<p><b>====== Задание 1 ======</b></p>'.PHP_EOL;
+
+$x = ['One','Two','Three'];
+
+echo '====== Часть 1 ======'.PHP_EOL;
+
+task1($x);
+
+echo '====== Часть 2 ======'.PHP_EOL;
+echo PHP_EOL;
+
+$y = task1($x, true);
+
+echo $y.PHP_EOL;
+
+/** Задание 2 */
+echo PHP_EOL;
+echo '<p><b>====== Задание 2 ======</b></p>'.PHP_EOL;
+
+$sym = ['+','-','*','/','%'];
+$x1 = array_rand($sym, 1);
+$x2 = rand(1, 10);
+$x3 = rand(1, 10);
+$x4 = (float)(rand(1, 10).'.'.rand(0, 9));
+
+task2($sym[$x1], $x2, 2, $x3, $x4);
+
+echo PHP_EOL;
+
+/** Задание 3 */
+echo PHP_EOL;
+echo '<p><b>====== Задание 3 ======</b></p>'.PHP_EOL;
+
+$sym = ['a',-2,-1,0,1,2,'b',3,4,5,6,7,8,9,'c',10];
+$x1 = array_rand($sym, 1);
+$x2 = array_rand($sym, 1);
+
+echo 'Количество строк: '.$sym[$x1].PHP_EOL;
+echo 'Количество столбцов: '.$sym[$x2].PHP_EOL;
+echo PHP_EOL;
+task3($sym[$x1], $sym[$x2]);
+
+echo PHP_EOL;
+
+/** Задание 4 */
+echo PHP_EOL;
+echo '<p><b>====== Задание 4 ======</b></p>'.PHP_EOL;
+
+echo '====== Часть 1 ======'.PHP_EOL;
+echo PHP_EOL;
+
+$date = task4();
+
+echo 'Текущая дата/время: '.$date.PHP_EOL;
+
+echo PHP_EOL;
+
+echo '====== Часть 2 ======'.PHP_EOL;
+
+echo PHP_EOL;
+
+$time = '24.02.2016 00:00:00';
+echo 'Запрашиваемое время: '.$time.PHP_EOL;
+
+$date = task4($time);
+echo 'Запрашиваемое время в UNIXTIME: '.$date.PHP_EOL;
+
+/** Задание 5 */
+echo PHP_EOL;
+echo '<p><b>====== Задание 5 ======</b></p>'.PHP_EOL;
+
+echo '====== Часть 1 ======'.PHP_EOL;
+echo PHP_EOL;
+
+$str = 'Карл у Клары украл Кораллы';
+$zam = 'К';
+$na = '';
+$rez = task5($str, $zam, $na);
+
+echo 'Исходная строка: "'.$str.'"'.PHP_EOL;
+echo 'Заменяем "'.$zam.'" на "'.$na.'"'.PHP_EOL;
+echo 'Результат: "'.$rez.'"'.PHP_EOL;
+
+echo PHP_EOL;
+
+echo '====== Часть 2 ======'.PHP_EOL;
+echo PHP_EOL;
+
+$str = 'Две бутылки лимонада';
+$zam = 'Две';
+$na = 'Три';
+$rez = task5($str, $zam, $na);
+
+echo 'Исходная строка: "'.$str.'"'.PHP_EOL;
+echo 'Заменяем "'.$zam.'" на "'.$na.'"'.PHP_EOL;
+echo 'Результат: "'.$rez.'"'.PHP_EOL;
+
+/** Задание 6 */
+echo PHP_EOL;
+echo '<p><b>====== Задание 6 ======</b></p>'.PHP_EOL;
+
+echo '====== Часть 1 ======'.PHP_EOL;
+echo PHP_EOL;
+
+$filename = 'text.txt';
+$text1 = 'Hello again!';
+
+file_put_contents($filename, $text1);
+
+if (file_exists($filename)) {
+    echo 'Файл успешно создан!'.PHP_EOL;
+    $text2 = file_get_contents($filename);
+    if ($text1 == $text2) {
+        echo 'Текст "'.$text1.'" успешно записан в файл!'.PHP_EOL;
+    } else {
+        echo 'Хм, текст почему-то отличается от запланированного!'.PHP_EOL;
+    }
+} else {
+    echo 'Хм, почему-то файл создать не удалось!'.PHP_EOL;
+}
+
+echo PHP_EOL;
+
+echo '====== Часть 2 ======'.PHP_EOL;
+echo PHP_EOL;
+
+$filename = 'text.txt';
+task6($filename);
---
+++ dz2/src/functions.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: walay
+ * Date: 20.08.2018
+ * Time: 14:15
+ */
+
+function task1($mass, $flag = null)
+{
+    $rez = '';
+    if ($flag) {
+        foreach ($mass as $mas) {
+            if (empty($rez)) {
+                $rez = $mas;
+            } else {
+                $rez = $rez . ' ' . $mas;
+            }
+        }
+    } else {
+        foreach ($mass as $mas) {
+            echo '<p>' . $mas . '</p>';
+        }
+    }
+    return $rez;
+};
+
+function task2(...$mass)
+{
+    $i = 0;
+    $op = 0;
+    $rez = 0;
+    $err = 0;
+    $sym = ['+','-','*','/','%'];
+
+    if (in_array($mass[0], $sym)) {
+        foreach ($mass as $mas) {
+            if (($i != 0) && (is_int($mas) || is_float($mas))) {
+                if (($i == 1)) {
+                    $op = $mas;
+                } elseif (($i != 0)) {
+                    $op = $op.' '.$mass[0].' '.$mas;
+                }
+            } elseif (($i != 0)) {
+                $err = 1;
+                echo 'Одно из значений не является числом!'.PHP_EOL;
+                break;
+            }
+            $i++;
+        }
+    } else {
+        $err = 1;
+        echo 'Певрое значение - не знак арифметической операции!'.PHP_EOL;
+    }
+    if ($err == 0) {
+        eval("\$rez= $op;");
+    } else {
+        echo 'В ходе выполнения операции возникла ошибка!'.PHP_EOL;
+    }
+
+    echo $op.' = '.$rez;
+    return $rez;
+};
+
+function task3($x1, $x2)
+{
+    if (is_int($x1) && (is_int($x2)) && ($x1 > 0) && ($x2 > 0)) {
+        echo '<table border="1">';
+        for ($i = 0; $i <= $x1; $i++) {
+            echo '<tr align="center">';
+            for ($j = 0; $j <= $x2; $j++) {
+                if (($i == 0) && ($j == 0)) {
+                    echo '<td bgcolor="#d3d3d3">';
+                    echo '';
+                    echo '</td>';
+                } elseif ($i == 0) {
+                    echo '<td bgcolor="#d3d3d3">';
+                    echo $j;
+                    echo '</td>';
+                } elseif ($j == 0) {
+                    echo '<td bgcolor="#d3d3d3">';
+                    echo $i;
+                    echo '</td>';
+                } else {
+                    echo '<td>';
+                    echo $i * $j;
+                    echo '</td>';
+                }
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+    } elseif ((is_int($x1)) && (is_int($x2)) && (($x1 <= 0) && ($x2 <= 0))) {
+        echo 'Количество строк и столбцов меньше либо равно 0!'.PHP_EOL;
+        echo 'Построение таблицы не возможно!';
+    } elseif ((is_int($x1)) && (is_int($x2)) && ($x1 <= 0)) {
+        echo 'Количество строк меньше либо равно 0!'.PHP_EOL;
+        echo 'Построение таблицы не возможно!';
+    } elseif ((is_int($x1)) && (is_int($x2)) && ($x2 <= 0)) {
+        echo 'Количество столбцов меньше либо равно 0!'.PHP_EOL;
+        echo 'Построение таблицы не возможно!';
+    } elseif ((!is_int($x1)) && (is_int($x2)) && ($x2 <= 0)) {
+        echo 'Количество строк не является целым числом!'.PHP_EOL;
+        echo 'Количество столбцов меньше либо равно 0!'.PHP_EOL;
+        echo 'Построение таблицы не возможно!';
+    } elseif ((is_int($x1)) && (!is_int($x2)) && ($x1 <= 0)) {
+        echo 'Количество строк меньше либо равно 0!'.PHP_EOL;
+        echo 'Количество столбцов не является целым числом!'.PHP_EOL;
+        echo 'Построение таблицы не возможно!';
+    } elseif (!is_int($x1) && (!is_int($x2))) {
+        echo 'Количество строк и столбцов не является целым числом!'.PHP_EOL;
+        echo 'Построение таблицы не возможно!';
+    } elseif (!is_int($x1)) {
+        echo 'Количество строк не является целым числом!'.PHP_EOL;
+        echo 'Построение таблицы не возможно!';
+    } else {
+        echo 'Количество столбцов не является целым числом!'.PHP_EOL;
+        echo 'Построение таблицы не возможно!';
+    }
+};
+
+function task4($time = null)
+{
+    if (empty($time)) {
+        $date = date("d.m.Y H:i");
+    } else {
+        $date = strtotime($time);
+    }
+    return $date;
+};
+
+function task5($st, $bkv, $zam = "")
+{
+    $str = str_replace($bkv, $zam, $st);
+    return $str;
+};
+
+function task6($file)
+{
+    if (file_exists($file)) {
+        $text = file_get_contents($file);
+        echo 'Указанный файл найден!'.PHP_EOL;
+        echo 'Его содержимое: "'.$text.'"'.PHP_EOL;
+    } else {
+        echo 'К сожалению, указанного файла не существует!';
+    }
+};
