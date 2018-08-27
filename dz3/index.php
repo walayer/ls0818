381810f254015a9ecb37797189b6cc30e1842709
Your Name <you@example.com>
2018-08-28 01:26:16 +0600

    Домашнее задание 3 index.php - основной файл, из которого осуществляется вызов функций functions.php - файл с набором функций data.xml - данные для первого задания

---
+++ dz3/index.php
@@ -0,0 +1,95 @@
+<pre>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: walay
+ * Date: 27.08.2018
+ * Time: 14:14
+ */
+
+require('src/functions.php');
+
+/** Задание 1 */
+/**
+Написать скрипт, который выведет всю информацию из файла data.xml в удобно
+читаемом виде.
+ */
+
+echo '<p><b>====== Задание 1 ======</b></p>'.PHP_EOL;
+
+task1('data.xml');
+
+echo PHP_EOL;
+
+/** Задание 2 */
+/**
+1. Создайте массив, в котором имеется как минимум 1 уровень вложенности.
+Преобразуйте его в JSON. Сохраните как output.json
+2. Откройте файл output.json. Случайным образом, используя функцию rand(), решите
+изменять данные или нет. Сохраните как output2.json
+3. Откройте оба файла. Найдите разницу и выведите информацию об отличающихся
+элементах
+ */
+
+echo '<p><b>====== Задание 2 ======</b></p>'.PHP_EOL;
+
+echo '====== Часть 1 ======'.PHP_EOL;
+
+$array = [
+        [
+            [rand(0, 100),rand(0, 100),rand(0, 100)],
+            [rand(0, 100),rand(0, 100),rand(0, 100)],
+            [rand(0, 100),rand(0, 100),rand(0, 100)]
+        ],
+        [
+            [rand(0, 100),rand(0, 100),rand(0, 100)],
+            [rand(0, 100),rand(0, 100),rand(0, 100)],
+            [rand(0, 100),rand(0, 100),rand(0, 100)]
+        ],
+];
+
+echo PHP_EOL;
+
+$filename1 = task2_1($array);
+
+
+echo PHP_EOL;
+echo '====== Часть 2 ======'.PHP_EOL;
+echo PHP_EOL;
+
+$flag = rand(0, 1);
+$filename2 = task2_2($filename1, $flag);
+
+echo PHP_EOL;
+echo '====== Часть 3 ======'.PHP_EOL;
+echo PHP_EOL;
+
+task2_3($filename1, $filename2);
+
+/** Задание 3 */
+/**
+1. Программно создайте массив, в котором перечислено не менее 50 случайных чисел
+от 1 до 100
+2. Сохраните данные в файл csv
+3. Откройте файл csv и посчитайте сумму четных чисел
+ */
+
+echo '<p><b>====== Задание 3 ======</b></p>'.PHP_EOL;
+
+task3_1(50);
+
+/** Задание 4 */
+/**
+1. С помощью PHP запросить данные по адресу:
+https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json
+2. Вывести title и page_id
+ */
+
+echo PHP_EOL;
+
+echo '<p><b>====== Задание 4 ======</b></p>'.PHP_EOL;
+
+task4_1('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json');
+
+?>
+</pre>
---
+++ dz3/src/functions.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: walay
+ * Date: 27.08.2018
+ * Time: 14:15
+ */
+
+/** Функция выводит всю информацию из передаваемого файла в удобно читаемом виде */
+
+function task1($filename)
+{
+    $file = file_get_contents($filename);
+    $xml = new SimpleXMLElement($file);
+
+    echo '**********************************************'.PHP_EOL;
+    echo 'PurchaseOrderNumber: '.$xml->attributes()->PurchaseOrderNumber[0].PHP_EOL;
+    echo 'OrderDate:  '.$xml->attributes()->OrderDate[0].PHP_EOL;
+    echo '**********************************************'.PHP_EOL;
+    echo 'Address '.$xml->Address[0]->attributes()->Type.': '.PHP_EOL;
+    foreach ($xml->Address[0] as $key => $value) {
+        echo $key.': '.$value.PHP_EOL;
+    }
+    echo '**********************************************'.PHP_EOL;
+    echo 'Address '.$xml->Address[1]->attributes()->Type.': '.PHP_EOL;
+    foreach ($xml->Address[1] as $key => $value) {
+        echo $key.': '.$value.PHP_EOL;
+    }
+    echo '**********************************************'.PHP_EOL;
+    echo 'DeliveryNotes: '.$xml->DeliveryNotes[0].PHP_EOL;
+    echo PHP_EOL;
+    echo 'Items '.$xml->Items->Item[0]->attributes()->PartNumber.': '.PHP_EOL;
+    foreach ($xml->Items->Item[0] as $key => $value) {
+        echo $key.': '.$value.PHP_EOL;
+    }
+    echo '**********************************************'.PHP_EOL;
+    echo 'Items '.$xml->Items->Item[1]->attributes()->PartNumber.': '.PHP_EOL;
+    foreach ($xml->Items->Item[1] as $key => $value) {
+        echo $key.': '.$value.PHP_EOL;
+    }
+    echo '**********************************************'.PHP_EOL;
+}
+
+/** Функция преобразует передаваемый массив в JSON-файл и записывает его в файл output.json */
+
+function task2_1($array)
+{
+    $encoded = json_encode($array, JSON_UNESCAPED_UNICODE);
+    $filename = 'output.json';
+    file_put_contents($filename, $encoded);
+
+    echo 'Файл '.$filename.' успешно создан!'.PHP_EOL;
+
+    return $filename;
+}
+
+/** Функция принимает JSON-файл, в зависимости от передаваемого второго параметра или изменяет рандомное число
+ * в массиве и записывает результат в файл output2.json
+ */
+
+function task2_2($filename, $flag)
+{
+    $file = file_get_contents($filename);
+    $decoded1 = json_decode($file, true);
+    $decoded2 = $decoded1;
+    if ($flag) {
+        echo 'Данные будут изменены!' . PHP_EOL;
+
+        $x1 = rand(0, 1);
+        $x2 = rand(0, 2);
+        $x3 = rand(0, 2);
+
+        $decoded2[$x1][$x2][$x3] = rand(0, 100);
+    } else {
+        echo 'Данные НЕ будут изменены!' . PHP_EOL;
+    }
+    $encoded2 = json_encode($decoded2, JSON_UNESCAPED_UNICODE);
+    $filename2 = 'output2.json';
+    file_put_contents($filename2, $encoded2);
+    echo 'Файл ' . $filename2 . ' успешно создан!' . PHP_EOL;
+
+    return $filename2;
+}
+
+/** Функция принимает два JSON-файла, сравнивает их и выводит различие, если оно есть */
+
+function task2_3($filename1, $filename2)
+{
+
+    $file1 = file_get_contents($filename1);
+    $decoded1 = json_decode($file1, true);
+    $file2 = file_get_contents($filename2);
+    $decoded2 = json_decode($file2, true);
+
+    echo 'Массив '.$filename1.'  '.json_encode($decoded1, JSON_UNESCAPED_UNICODE).PHP_EOL;
+    echo 'Массив '.$filename2.' '.json_encode($decoded2, JSON_UNESCAPED_UNICODE).PHP_EOL;
+//    echo $encoded1.PHP_EOL;
+//    echo $encoded2.PHP_EOL;
+    $difference = false;
+    foreach ($decoded1 as $key1_1 => $decode1) {
+        foreach ($decoded2 as $key2_1 => $decode2) {
+            foreach ($decode1 as $key1_2 => $decod1) {
+                foreach ($decode2 as $key2_2 => $decod2) {
+                    foreach ($decod1 as $key1_3 => $deco1) {
+                        foreach ($decod2 as $key2_3 => $deco2) {
+                            if (($key1_1 == $key2_1) && ($key1_2 == $key2_2) && ($key1_3 == $key2_3)) {
+                                if ($deco1 != $deco2) {
+                                    echo '<p>Различающиеся элементы: <b>'.$deco1 . ' ' . $deco2.'</b></p>'.PHP_EOL;
+                                    $difference = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (!$difference) {
+        echo 'Различающихся элементов НЕТ!'.PHP_EOL;
+    }
+}
+
+
+/** Функция принимает размер создаваемого рандомного массива, сохраняет его в файл output.csv и
+ * считает сумму четных чисел в нём
+ */
+
+function task3_1($size)
+{
+    for ($i = 1; $i <= $size; $i++) {
+        $array[] = rand(1, 100);
+    }
+
+    echo 'Массив из '.$size.' элементов: '.json_encode($array, JSON_UNESCAPED_UNICODE).PHP_EOL;
+
+    $filename = 'output.csv';
+    $fp = fopen($filename, 'w');
+    fputcsv($fp, $array, ';');
+    fclose($fp);
+
+    echo 'Файл '.$filename.' успешно создан!'.PHP_EOL;
+
+    $fp = fopen($filename, 'r');
+    $result = 0;
+    $formula = '';
+
+    if ($fp) {
+        while (($csvData = fgetcsv($fp, 1000, ";")) !== false) {
+            echo 'Сумма четных чисел: ';
+            foreach ($csvData as $element) {
+                if ($element % 2 == 0) {
+                    $formula = $formula.' '.$element;
+                    $result += $element;
+                }
+            }
+        }
+        echo $formula.' = '.$result.PHP_EOL;
+    }
+
+    fclose($fp);
+}
+
+
+/** Функция принимает адрес сайта и выводит title и page_id
+ */
+
+function task4_1($address)
+{
+    $data = file_get_contents($address);
+    $decoded = json_decode($data, true);
+
+    echo 'Page ID: '.$decoded['query']['pages']['15580374']['pageid'].PHP_EOL;
+    echo 'Title: '.$decoded['query']['pages']['15580374']['title'].PHP_EOL;
+    echo PHP_EOL;
+}
